6fcdbf35f9dbad51aecdd43e5297325cfb60078a
MiJunior <38766807+MiJunior@users.noreply.github.com>
2018-09-02 16:27:21 +0300

    Головна сторінка

--- index.php
+++ index.php
@@ -1,2 +1,49 @@
 <?php
-require 'db_connect.php';
+session_start();
+require_once 'db/db_connector.php';
+require_once  'parts/head.php';
+require_once 'showpost.php';
+
+?>
+
+<div class="container">
+    <?php foreach ($post as $show){?>
+    <div class="card" style="margin:10px">
+        <?php if(!empty($show['name'])) {?>
+        <div class="card-header">
+        <?=$show['name'];?>
+        </div>
+        <?php }?>
+        <div class="card-header">
+            <?=$show['email'];?>
+        </div>
+
+        <div class="card-body">
+            <p class="card-text"><?=$show['text'];?></p>
+            <?php if(!empty($show['image_path'])) {?>
+            <img src="<?=$show['image_path']?>" alt="">
+            <?php }?>
+        </div>
+    </div>
+    <?php } ?>
+    <hr>
+    <form action="controllers/addpost.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <label for="InputName">Name</label>
+            <input type="text" name="name" class="form-control" placeholder="Name">
+        </div>
+        <div class="form-group">
+            <label for="Input Text">Content</label>
+            <input type="text" name="content" class="form-control" placeholder="Content">
+        </div>
+        <p><input type="file" name="image">
+        <hr>
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </form>
+</div>
+
