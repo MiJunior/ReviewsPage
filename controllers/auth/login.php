fbfce90ec8492e235a9fba32f2c001415c0c6354
MiJunior <38766807+MiJunior@users.noreply.github.com>
2018-09-02 16:20:17 +0300

    Обробники авторизації

---
+++ controllers/auth/login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once '../lib/db_connector.php';
+
+if(!empty($_SESSION["session_username"])){
+    // вывод "Session is set"; // в целях проверки
+    header("Location: index.php");
+}
+if(isset($_POST['doLogin'])){
+    if(!empty($_POST['login'] && !empty($_POST['password']))){
+        $username = htmlspecialchars(trim($_POST['login']));
+        $password = md5(trim(htmlspecialchars($_POST['password'])));
+        $query = $pdo->prepare("SELECT * FROM `users` 
+                  WHERE  `login` = (:login) && `password` = (:password) ");
+        $query->execute(['login'=> $username, 'password'=> $password]);
+        $num_rows_login = $query->fetchColumn();
+        $role = $pdo->prepare("SELECT * FROM `users` 
+                                WHERE `login` = (:login) && `password` = (:password)");
+        $role->execute(['login'=> $username, 'password'=> $password]);
+        $role_id = $role->fetch(PDO::FETCH_ASSOC);
+        if($num_rows_login!=0){
+            $_SESSION['session_username']=$username;
+            $_SESSION['role_id'] = $role_id['role_id'];
+            header("Location: ../index.php");
+            }else {
+                echo "Invalid login or password";
+            }
+        }
+
+
+}
---
+++ controllers/auth/reg.php
@@ -0,0 +1,53 @@
+<?php
+require_once '../lib/db_connector.php';
+if(isset($_POST['doReg'])){
+    $errors = [];
+
+    //Проверка существования данных
+    if(!empty($_POST['login']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm'])){
+        $username = htmlspecialchars(trim($_POST['login']));
+        $password = md5(trim(htmlspecialchars($_POST['password'])));
+        $email = htmlspecialchars(trim($_POST['email']));
+
+        //совпадают ли пароли
+        if($_POST['password'] != $_POST['confirm']){
+            die('Passwords do not match');//пароли не совпадают
+        }
+
+        //валидация почты
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            die('Email is not correctly. example@domain.com');
+        }
+
+        //проверка уникальности логина
+        $uniq_login = $pdo->prepare("SELECT * FROM `users` 
+                                WHERE `login` = (:login)");
+        $uniq_login->execute(['login' => $username]);
+        $num_rows = $uniq_login->fetchColumn();
+        if($num_rows!=0){
+            die( 'This login is already taken');
+
+        }else
+        //проверка уникальности почты
+        $uniq_email = $pdo->prepare("SELECT * FROM `users` 
+                                WHERE `email` = (:email)");
+        $uniq_email->execute(['email'=> $email]);
+        $num_rows_email = $uniq_email->fetchColumn();
+        if($num_rows_email!=0){
+            die('This email is already taken');
+        }
+        print_r($errors);
+        try{
+            $query = $pdo->prepare("INSERT INTO `users` (`id`, `login`, `email`, `password`) 
+                          VALUES (NULL, :login, :email, :password)");
+            $success = $query->execute(['login'=> $username, 'email' => $email, 'password'=> $password]);
+            header("Location: ../index.php");
+
+        }catch (Exception $e){
+        die( "Error: ".$e);
+        }
+    }
+    echo "All fields are required!";
+
+
+}
