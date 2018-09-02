8b5b3b8d2b148e241c767cfd17fd9eec71a55a10
MiJunior <38766807+MiJunior@users.noreply.github.com>
2018-09-02 16:27:50 +0300

    Сторінка авторизації

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once 'db/db_connector.php';
+require_once  'parts/head.php';
+require_once 'showpost.php';
+if(!empty($_SESSION["session_username"])){
+    // вывод "Session is set"; // в целях проверки
+    header("Location: index.php");
+}
+?>
+<div class="container">
+    <form action="controllers/auth/login.php" method="post">
+        <div class="form-group">
+            <label >Login</label>
+            <input type="text"  name="login"  class="form-control"  placeholder="Login">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password"  name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" name="doLogin" class="btn btn-primary">Submit</button>
+    </form>
+</div>
